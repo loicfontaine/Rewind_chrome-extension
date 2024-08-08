ffcf8d8397b6a0b97ea35a0baa14d3c13de2157c
Loïc Fontaine <loic.fontaine@heig-vd.ch>
2024-08-08 14:37:43 +0200

    Visual changes

--- background.js
+++ background.js
@@ -1,70 +1,12 @@
-//const monitoredKeywords = ["example", "mozilla", "specificKeyword"]; // Add your keywords here
-/*
-function isMonitoredUrl(url) {
-  //return monitoredKeywords.some((keyword) => url.includes(keyword));
-  return true;
-}
-
-// Store the time spent data locally
-function storeTimeSpent(pageUrl, timeSpent) {
-  console.log("storeTimeSpent", "pageUrl", pageUrl, "timeSpent", timeSpent);
-  browser.storage.local.get("timeSpentData").then((result) => {
-    const timeSpentData = result.timeSpentData || {};
-
-    if (timeSpentData[pageUrl]) {
-      timeSpentData[pageUrl] += timeSpent;
-    } else {
-      timeSpentData[pageUrl] = timeSpent;
-    }
-
-    browser.storage.local.set({ timeSpentData });
-  });
-}
-
-// Receive messages from the content script
-browser.runtime.onMessage.addListener((message) => {
-  if (
-    message.from === "contentScript" &&
-    message.subject === "TimeSpentOnPage"
-  ) {
-    const pageUrl = message.pageUrl;
-    const timeSpent = message.timeSpent;
-
-    storeTimeSpent(pageUrl, timeSpent);
-  }
-});
-
-// Fetch browsing history and combine it with the time spent data
-function getHistoryWithTimeSpent(callback) {
-  browser.history.search({ text: "", maxResults: 100 }).then((historyItems) => {
-    browser.storage.local.get("timeSpentData").then((result) => {
-      const timeSpentData = result.timeSpentData || {};
-      const combinedData = historyItems
-        .filter((item) => isMonitoredUrl(item.url))
-        .map((item) => {
-          const timeSpent = timeSpentData[item.url] || 0;
-          return {
-            ...item,
-            timeSpent,
-            timeSpentInSeconds: (timeSpent / 1000).toFixed(2),
-          };
-        });
-
-      callback(combinedData);
-    });
-  });
-}
-
-
-*/
-
 let startTime = null;
 let totalTimeSpent = 0;
 let currentUrl = "";
+let currentTitle = "";
 
-function startTimer(url) {
+function startTimer(url, title) {
   startTime = Date.now();
   currentUrl = url;
+  currentTitle = title;
 }
 
 function stopTimer() {
@@ -72,65 +14,55 @@ function stopTimer() {
     totalTimeSpent += Date.now() - startTime;
     startTime = null;
     // Enregistrer le temps passé sur l'URL actuelle
-    storeTimeSpent(currentUrl, totalTimeSpent);
+    if (totalTimeSpent > 1000) {
+      storeTimeSpent(currentUrl, totalTimeSpent);
+    }
     totalTimeSpent = 0; // Réinitialiser pour la prochaine utilisation
   }
 }
 
 function storeTimeSpent(pageUrl, timeSpent) {
-  console.log(`Time spent on ${pageUrl}: ${timeSpent / 1000} seconds`);
-  browser.storage.local.get("timeSpentData").then((result) => {
-    const timeSpentData = result.timeSpentData || {};
-
-    if (timeSpentData[pageUrl]) {
-      timeSpentData[pageUrl] += timeSpent;
-    } else {
-      timeSpentData[pageUrl] = timeSpent;
-    }
-
-    browser.storage.local.set({ timeSpentData });
+  chrome.storage.local.get("timeSpentData", (result) => {
+    const timeSpentData = result.timeSpentData || [];
+    timeSpentData.push({
+      pageUrl: pageUrl,
+      timeSpent: timeSpent,
+      title: currentTitle,
+      startTime: Date.now() - timeSpent,
+      endTime: Date.now(),
+    });
+    chrome.storage.local.set({ timeSpentData });
   });
 }
 
-browser.windows.onFocusChanged.addListener((windowId) => {
-  if (windowId === browser.windows.WINDOW_ID_NONE) {
+chrome.windows.onFocusChanged.addListener((windowId) => {
+  if (windowId === chrome.windows.WINDOW_ID_NONE) {
     // Le navigateur a perdu le focus
     isBrowserFocused = false;
     stopTimer();
   } else {
     // Le navigateur a reçu le focus
-    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs[0]) {
         isBrowserFocused = true;
-        startTimer(tabs[0].url);
+        startTimer(tabs[0].url, tabs[0].title);
       }
     });
   }
 });
 
 // Écouter les changements d'onglet
-browser.tabs.onActivated.addListener((activeInfo) => {
+chrome.tabs.onActivated.addListener((activeInfo) => {
   stopTimer(); // Arrêter le timer sur l'ancien onglet
-  browser.tabs.get(activeInfo.tabId, (tab) => {
-    startTimer(tab.url); // Démarrer le timer sur le nouvel onglet
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    startTimer(tab.url, tab.title); // Démarrer le timer sur le nouvel onglet
   });
 });
 
 // Écouter les mises à jour d'onglet (pour les changements d'URL dans le même onglet)
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.url) {
     stopTimer(); // Arrêter le timer car l'URL a changé
-    startTimer(changeInfo.url); // Démarrer le timer avec la nouvelle URL
-  }
-});
-
-// Make the data retrievable for the popup
-browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (
-    message.from === "popup" &&
-    message.subject === "GetHistoryWithTimeSpent"
-  ) {
-    getHistoryWithTimeSpent(sendResponse);
-    return true; // Required to use asynchronous `sendResponse`
+    startTimer(changeInfo.url, changeInfo.title); // Démarrer le timer avec la nouvelle URL
   }
 });
